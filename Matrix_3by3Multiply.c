77b648f598f6a830802fbc1952bc9bb896272d82
Kusiyait Krishna <krishkusiyait@gmail.com>
2024-02-02 22:38:20 +0545

    Create Matrix_3by3Multiply.c

---
+++ Matrix_3by3Multiply.c
@@ -0,0 +1,69 @@
+// c program to find multiplication of 3 by 3 matrix using array of array
+
+#include <stdio.h>
+int main()
+{
+    int n = 3;
+    int matrix1[n][n];
+    int matrix2[n][n];
+    int matrix3[n][n];
+    // entering the element of first matrix using array of array
+    printf("Enter the element of first matrix:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("Enter the element of row %d x %d: ", i + 1, j + 1);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+    // entering the element of second matrix using array of array
+    printf("Enter the element of second matrix:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("Enter the element of row %d x %d: ", i + 1, j + 1);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+    // displaying the first and second matrix using array of array
+    printf("The first matrix is:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d\t", matrix1[i][j]);
+        }
+        printf("\n");
+    }
+    // displaying the first and second matrix using array of array
+    printf("The second matrix is:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d\t", matrix2[i][j]);
+        }
+        printf("\n");
+    }
+    // performing the operation to find the multiplication of the matrix
+    printf("The multiplication of the matrix is:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix3[i][j] = 0;
+            for (int k = 0; k < n; k++)
+            {
+                /*multiplying the element of the first matrix with the element of the second matrix 
+                and adding them to the element of the third matrix 
+                */
+                matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+            printf("%d\t", matrix3[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
